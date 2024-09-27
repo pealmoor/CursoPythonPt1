32b6af280b12a1a4370413130843bf1f7968c0e6
Pedro <pealmo98@gmail.com>
2024-09-27 13:03:29 -0500

    Clase 9: Matrices, tuplas

---
+++ clase9.py
@@ -0,0 +1,22 @@
+
+#Matrix
+matrix=[[1,2,3],
+        [4,5,6],
+        [7,8,9]]    
+print(matrix)
+print(matrix[0])
+print(matrix[2])
+print(matrix[0][2])
+
+#Tupla
+numbers=(1,2,3,4,5)
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+
+"""""
+No se puede en tuplas
+numbers[0]="Uno"
+print[numbers]
+
+"""
